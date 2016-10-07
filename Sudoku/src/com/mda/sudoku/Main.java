fcb9bb55fd34361e683d4792d876380e4a6248e5
mda06 <michaelvm171@msn.com>
2016-10-07 17:14:16 +0200

    Added solve with blacktracking

--- Sudoku/src/com/mda/sudoku/Main.java
+++ Sudoku/src/com/mda/sudoku/Main.java
@@ -5,34 +5,39 @@ import java.util.Scanner;
 public class Main {
 	private static Sudoku sud = new Sudoku();
 	private static Scanner scan = new Scanner(System.in);
-	
+
 	private static boolean handleInput() {
 		System.out.print("\nPlease enter value,x,y : ");
 		String in = scan.nextLine();
-		if(in.equals("stop")) return true;
-		if(in.equals("solve")) {
-			sud.solve();
+		if (in.equals("stop")) return true;
+		if (in.equals("solve")) {
+			System.out.print("Which type ? Bruteforce(1), Blacktracing(2): ");
+			in = scan.nextLine();
+			if(in.equals("1")) sud.solve();
+			else if(in.equals("2")) sud.solveBlacktracking();
+			else System.out.println("Bad entry !");
+			System.out.println();
 			return false;
 		}
 		System.out.println();
-		
+
 		String[] split = in.split(",");
-		if(split.length == 3) {
+		if (split.length == 3) {
 			try {
 				int v = Integer.valueOf(split[0]), x = Integer.valueOf(split[1]), y = Integer.valueOf(split[2]);
 				sud.put(v, x, y);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.err.println("Bad entry !");
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static void main(String[] args) {
-		while(true) {
+		while (true) {
 			sud.print();
-			if(handleInput()) break;
+			if (handleInput()) break;
 		}
 	}
 }
--- Sudoku/src/com/mda/sudoku/Sudoku.java
+++ Sudoku/src/com/mda/sudoku/Sudoku.java
@@ -1,27 +1,35 @@
 package com.mda.sudoku;
 
-
-
 public class Sudoku {
 	private int[][] grid;
 	private boolean showSolve;
-	
+
 	public Sudoku() {
 		showSolve = false;
-
 		initGrid();
 	}
-	
+
+	public Sudoku(Sudoku other) {
+		grid = new int[9][9];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				this.grid[i][j] = other.grid[i][j];
+			}
+		}
+	}
+
+	public Sudoku(int[][] grid) {
+		this.grid = new int[9][9];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				this.grid[i][j] = grid[i][j];
+			}
+		}
+	}
+
 	public void initGrid() {
 		grid = new int[9][9];
-		
-		//Random rand = new Random();
-		/*for(int i = 0; i < 30; i++) {
-			int x = rand.nextInt(grid.length), y = rand.nextInt(grid[0].length), v = 1 + rand.nextInt(9);
-			if(isValid(v, x, y)) put(v, x, y);
-			else i--;
-		}*/
-		
+
 		grid[0][0] = 5;
 		grid[1][0] = 3;
 		grid[4][0] = 7;
@@ -53,20 +61,22 @@ public class Sudoku {
 		grid[7][8] = 7;
 		grid[8][8] = 9;
 	}
-	
+
 	public void print() {
 		System.out.print("    ");
-		for(int i = 0; i < grid.length; i++) System.out.print(i + " ");
+		for (int i = 0; i < grid.length; i++)
+			System.out.print(i + " ");
 		System.out.println();
 		System.out.print("    ");
-		for(int i = 0; i < grid.length; i++) System.out.print("! ");
+		for (int i = 0; i < grid.length; i++)
+			System.out.print("! ");
 		System.out.println();
-			
-		for(int y = 0; y < grid[0].length; y++) {
+
+		for (int y = 0; y < grid[0].length; y++) {
 			System.out.print(y + "-> ");
-			for(int x = 0; x < grid.length; x++) {
+			for (int x = 0; x < grid.length; x++) {
 				String v = String.valueOf(grid[x][y]);
-				if(v .endsWith("0")) v = ".";
+				if (v.endsWith("0")) v = ".";
 				System.out.print(v + (y == grid[x].length ? "" : "|"));
 			}
 			System.out.println();
@@ -74,136 +84,164 @@ public class Sudoku {
 	}
 
 	public void solve() {
-		for(int n = 0; n < 3; n++)
-		for(int i = 1; i < 10; i++)
-		while(blockSolve(i));
-		
-		for(int n = 0; n < 3; n++)
-		for(int i = 1; i < 10; i++)
-		while(rowSolve(i));
-		
-		for(int n = 0; n < 4; n++)
-		for(int i = 1; i < 10; i++)
-		while(colSolve(i));
+		for (int n = 0; n < 3; n++)
+			for (int i = 1; i < 10; i++) {
+				blockSolve(i);
+			}
+
+		for (int n = 0; n < 3; n++)
+			for (int i = 1; i < 10; i++) {
+				rowSolve(i);
+			}
+
+		for (int n = 0; n < 4; n++)
+			for (int i = 1; i < 10; i++) {
+				colSolve(i);
+			}
+	}
+
+	public void solveBlacktracking() {
+		solveBlacktracking(0, 0);
+	}
+
+	private boolean solveBlacktracking(int x, int y) {
+		if (x == 9) {
+			x = 0;
+			if (++y == 9) return true;
+		}
+		if (!isEmpty(x, y)) return solveBlacktracking(x + 1, y);
+
+		for (int val = 1; val <= 9; ++val) {
+			if (isValid(val, x, y)) {
+				put(val, x, y);
+
+				if (solveBlacktracking(x + 1, y)) return true;
+			}
+		}
+
+		put(0, x, y);
+		return false;
 	}
 	
+
 	public boolean blockSolve(int value) {
 		boolean ret = false;
-		
-		for(int n = 0; n < 9; n++) {
+
+		for (int n = 0; n < 9; n++) {
 			int bx = n % 3, by = n / 3;
 			int nbValid = 0, xValid = 0, yValid = 0;
-			for(int i = 0; i < 3; i++) {
-				for(int j = 0; j < 3; j++) {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
 					int x = i + bx * 3, y = j + by * 3;
-					if(isValid(value, x, y)) {
+					if (isValid(value, x, y)) {
 						nbValid++;
 						xValid = x;
 						yValid = y;
 					}
 				}
 			}
-			//If it can only goes in one position we put it on
-			if(nbValid == 1) {
+			// If it can only goes in one position we put it on
+			if (nbValid == 1) {
 				put(value, xValid, yValid);
 				ret = true;
-				if(showSolve) System.out.println("Block solver v:" + value + " at " + xValid + "," + yValid);
+				if (showSolve) System.out.println("Block solver v:" + value + " at " + xValid + "," + yValid);
 			}
 		}
 		return ret;
 	}
-	
+
 	public boolean rowSolve(int value) {
 		boolean ret = false;
-		
-		for(int y = 0; y < grid[0].length; y++) {
+
+		for (int y = 0; y < grid[0].length; y++) {
 			int nbValid = 0, xValid = 0;
-			for(int x = 0; x < grid.length; x++) { 
-				if(isValid(value, x, y)) { 
+			for (int x = 0; x < grid.length; x++) {
+				if (isValid(value, x, y)) {
 					nbValid++;
 					xValid = x;
 				}
 			}
-			//If it can only goes in one position we put it on
-			if(nbValid == 1) {
+			// If it can only goes in one position we put it on
+			if (nbValid == 1) {
 				put(value, xValid, y);
 				ret = true;
-				if(showSolve) System.out.println("Row solver v:" + value + " at " + xValid + "," + y);
+				if (showSolve) System.out.println("Row solver v:" + value + " at " + xValid + "," + y);
 			}
 		}
-		
+
 		return ret;
 	}
-	
+
 	public boolean colSolve(int value) {
 		boolean ret = false;
-		
-		for(int x = 0; x < grid.length; x++) {
+
+		for (int x = 0; x < grid.length; x++) {
 			int nbValid = 0, yValid = 0;
-			for(int y = 0; y < grid[0].length; y++) { 
-				if(isValid(value, x, y)) { 
+			for (int y = 0; y < grid[0].length; y++) {
+				if (isValid(value, x, y)) {
 					nbValid++;
 					yValid = y;
 				}
 			}
-			//If it can only goes in one position we put it on
-			if(nbValid == 1) {
+			// If it can only goes in one position we put it on
+			if (nbValid == 1) {
 				put(value, x, yValid);
 				ret = true;
-				if(showSolve) System.out.println("Col solver v:" + value + " at " + x + "," + yValid);
+				if (showSolve) System.out.println("Col solver v:" + value + " at " + x + "," + yValid);
 			}
 		}
-		
+
 		return ret;
 	}
+
 	
 	public void put(int value, int x, int y) {
-		if(isValid(value, x, y)) grid[x][y] = value;
+		grid[x][y] = value;
 	}
-	
-	public boolean isValid(int value, int x, int y) {
-		if(!isValidRow(value, y) || !isValidCol(value, x) || !isValidBlock(value, x, y)) return false;
-		return true;
-	}
-	
-	public boolean isValidBlock(int value, int x, int y) {
-		if(!checkBounds(x, y) || !isEmpty(x, y)) return false;
-		
-		int bx = x / 3, by = y / 3;
 
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(grid[i + bx * 3][j + by * 3] == value) return false;
+	public boolean isValid(int value, int x, int y) {
+		return isValidRow(value, y) && isValidCol(value, x) && isValidBlock(value, x, y);
+	}
+
+	public boolean isValidBlock(int value, int x, int y) {
+		if (!checkBounds(x, y)) return false;
+
+		int bx = (x / 3) * 3;
+		int by = (y / 3) * 3;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (grid[bx + i][by + j] == value) return false;
 			}
 		}
-		
+
 		return true;
 	}
 
 	public boolean isValidRow(int value, int y) {
-		if(!checkBounds(0, y)) return false;
+		if (!checkBounds(0, y)) return false;
 
-		for(int i = 0; i < grid[0].length; i++) 
-			if(value == grid[i][y]) return false;
-		
+		for (int i = 0; i < grid[0].length; i++)
+			if (value == grid[i][y]) return false;
+
 		return true;
 	}
-	
+
 	public boolean isValidCol(int value, int x) {
-		if(!checkBounds(x, 0)) return false;
-		
-		for(int i = 0; i < grid.length; i++) 
-			if(value == grid[x][i]) return false;
-		
+		if (!checkBounds(x, 0)) return false;
+
+		for (int i = 0; i < grid.length; i++)
+			if (value == grid[x][i]) return false;
+
 		return true;
 	}
-	
+
 	public boolean isEmpty(int x, int y) {
 		return grid[x][y] == 0;
 	}
-	
+
 	public boolean checkBounds(int x, int y) {
-		if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) return false;
+		if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) return false;
 		return true;
 	}
 }
